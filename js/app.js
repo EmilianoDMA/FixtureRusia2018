31e7fd415ff6ee6b1535aeb1463a53b200948244
ian <ianmazzaglia@gmail.com>
2018-04-13 19:16:22 -0300

    octavos en el js

--- js/app.js
+++ js/app.js
@@ -14,9 +14,11 @@ var app = (function(scope = {}) {
             $("td", $grupo).click(function() {
                 console.log("click en grupo", g.letra, g.cont_ganadores);
                 console.log($(this));
+                var nombre;
                 if (g.cont_ganadores <= 2){
                     $(this).attr("style", "background-color: #f42242");
                     $(this).attr("id", "seleccionado");
+                    $('[data-grupo='+g.letra+g.cont_ganadores+']').html($(this).html())
                     $(this).html(g.cont_ganadores + $(this).html());
                     g.cont_ganadores = g.cont_ganadores + 1;
                 }
