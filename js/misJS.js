1f51d8a6db3345150cd9ef44e910ea401dbea2c2
ian <ianmazzaglia@gmail.com>
2018-04-16 19:37:17 -0300

    cambios listos

--- js/misJS.js
+++ js/misJS.js
@@ -4,11 +4,38 @@ function mOver(obj) {
     }
 }
 
+
+function mOverBackground(obj) {
+	estilo = $(obj).attr('style');
+	console.log(estilo);
+	$(obj).attr('style', estilo + "background-blend-mode: unset; ");
+}
+
 function mOut(obj) {
 	if (obj.id != "seleccionado") {
         obj.style.backgroundColor = "#ffffff";
     } 
 }
+
+
+function mOutBackground(obj) {
+	estilo = $(obj).attr('style');
+	$(obj).attr('style', estilo + "background-blend-mode: color-burn; ");
+}
+
+var div;
+function select(obj) {
+	div = obj;
+	var numero = $(obj).attr('data-next');
+	console.log($(obj).html());
+	$('[data-cuar='+numero+']').html($(obj).html())
+}
+
+
+
+
+
+
 
 /*
 var ganadores = 1;
